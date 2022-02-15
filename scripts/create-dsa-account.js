46620933e11b4b6e0a935aab81c0db488dc6256f
Chinmay Chougaonkar <chinmay0402@gmail.com>
2022-02-15 14:00:50 +0530

    added cast spell in script

--- scripts/create-dsa-account.js
+++ scripts/create-dsa-account.js
@@ -1,20 +1,59 @@
-const Web3 = require('web3');
 const DSA = require('dsa-connect');
+require("@nomiclabs/hardhat-web3");
+require("hardhat")
 require('dotenv').config();
-const ETH_NODE_URL = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`;
-const web3 = new Web3(new Web3.providers.HttpProvider(ETH_NODE_URL)); // to enable web3 calls via SDK
 
-const dsa = new DSA({
-    web3: web3,
-    mode: "node",
-    privateKey: process.env.PRIVATE_KEY
-});
+const address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
 
-const buildAccount = async () => {
-    const account = await dsa.build({
-        gasPrice: web3.utils.toWei('0', 'ether') // estimated gas price
-        // origin: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-        // authority: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
-    });
+const setBalance = async () => {
+    await network.provider.send("hardhat_setBalance", [
+        address,
+        "0xD3C21BCECCEDA1000000"
+    ]);
 }
-buildAccount();
+
+setBalance();
+
+const castSpells = async () => {
+    const dsa = new DSA({
+        web3: web3,
+        mode: "node",
+        privateKey: process.env.PRIVATE_KEY
+    });
+    await dsa.build({
+        gasPrice: web3.utils.toWei('0.000000100000000000', 'ether'),// estimated gas price
+        origin: address,
+        authority: address
+    });
+
+    const accounts = await dsa.getAccounts(address);
+    // console.log(accounts[0].id);
+    const dsaId = accounts[0].id;
+    await dsa.setInstance(dsaId);
+    let spells = dsa.Spell({
+        gasPrice: web3.utils.toWei('0.000000100000000000', 'ether'),// estimated gas price
+    });
+
+    await spells.add({
+        connector: "compound",
+        method: "deposit",
+        args: [
+          "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+          "1000000000000000000", // 1 ETH (10^18 wei)
+          0,
+          0
+        ]
+      })
+    console.log(spells.data[0].args);
+    // let transactionHash = await spells.cast({
+    //     gasPrice: web3.utils.toWei('0.000000100000000000', 'ether'),// estimated gas price
+    // });
+    // console.log(transactionHash);
+
+    spells.cast({
+        gasPrice: web3.utils.toWei('1000000', 'gwei'), // in gwei, used in node implementation.
+        value: "10000000000000000000" // sending 1 Eth along the transaction.
+      }).then(console.log);
+}
+
+castSpells();
