3f2e419e940ebce2e6d4742286e5cb543ebfaffc
Chinmay Chougaonkar <chinmay0402@gmail.com>
2022-02-15 17:16:36 +0530

    fixed, refactored code, added comments

--- scripts/create-dsa-account.js
+++ scripts/create-dsa-account.js
@@ -3,57 +3,64 @@ require("@nomiclabs/hardhat-web3");
 require("hardhat")
 require('dotenv').config();
 
-const address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+const main = async () => {
+    const address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
 
-const setBalance = async () => {
+    // seed ethereum account with ether using hardhat's setBalance feature for mainnet forks
     await network.provider.send("hardhat_setBalance", [
         address,
-        "0xD3C21BCECCEDA1000000"
+        "0x21e19e0c9bab2400000"
     ]);
-}
 
-setBalance();
-
-const castSpells = async () => {
+    // instantiate dsa with web3 instance
     const dsa = new DSA({
         web3: web3,
         mode: "node",
         privateKey: process.env.PRIVATE_KEY
     });
+
+    // create a dsa account
     await dsa.build({
-        gasPrice: web3.utils.toWei('0.000000100000000000', 'ether'),// estimated gas price
-        origin: address,
-        authority: address
+        gasPrice: web3.utils.toWei('1000000', 'gwei'), // gas estimate, necessary when using NodeJs for calls
+        authority: address, // the address to be added as authority for the account
+        version: 2
     });
 
+    // fetch all dsa accounts owned by address 
     const accounts = await dsa.getAccounts(address);
-    // console.log(accounts[0].id);
-    const dsaId = accounts[0].id;
-    await dsa.setInstance(dsaId);
-    let spells = dsa.Spell({
-        gasPrice: web3.utils.toWei('0.000000100000000000', 'ether'),// estimated gas price
-    });
 
-    await spells.add({
-        connector: "compound",
+    const dsaId = accounts[0].id;
+
+    // set the (dsa) account which is to be used for subsequent dsa calls
+    await dsa.setInstance(dsaId);
+
+    let spells = dsa.Spell();
+
+    // provide ether to the DSA contract to be able to perform further operations
+    // ( if we skip this step, we need to provide ether in the "value" field in spells.cast() )
+    await network.provider.send("hardhat_setBalance", [
+        accounts[0].address,
+        ethers.utils.parseEther("10.0").toHexString() 
+        // NOTE: be careful while using above method (sometimes pads hex string with 0s which makes input invalid)
+    ])
+
+    // add a spell to cast
+    spells.add({ 
+        connector: "COMPOUND-A", // ID of the protocol which we want to interact with 
         method: "deposit",
         args: [
-          "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-          "1000000000000000000", // 1 ETH (10^18 wei)
-          0,
-          0
+            "ETH-A", // Id of the token to be used for interaction
+            "10000000000000000000", // amount of token (here, 10 ether = 10* (10**18) wei)
+            0, // getId
+            0 // setId (used along with getId to share data from one spell to another)
         ]
-      })
-    console.log(spells.data[0].args);
-    // let transactionHash = await spells.cast({
-    //     gasPrice: web3.utils.toWei('0.000000100000000000', 'ether'),// estimated gas price
-    // });
-    // console.log(transactionHash);
+    })
 
-    spells.cast({
+    // "cast" the spells
+    const transactionHash = await spells.cast({
         gasPrice: web3.utils.toWei('1000000', 'gwei'), // in gwei, used in node implementation.
-        value: "10000000000000000000" // sending 1 Eth along the transaction.
-      }).then(console.log);
+    });
+    console.log(transactionHash);
 }
 
-castSpells();
+main();
